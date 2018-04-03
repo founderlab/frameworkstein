985f0f36683ecc2c86f104c0134d0199e9954484
gwilym <gwilym@gwilymh.com>
2018-04-03 20:18:42 +1000

    Use prop-types

--- packages/fl-auth-react/src/RegisterForm.js
+++ packages/fl-auth-react/src/RegisterForm.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import {Button} from 'reactstrap'
 import {Input} from 'fl-react-utils'
 import {reduxForm, Field} from 'redux-form'
