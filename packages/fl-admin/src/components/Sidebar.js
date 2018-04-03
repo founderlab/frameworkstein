1d6715dfa4ab9ca50849a1dbbd4d8a128807af71
gwilym <gwilym@gwilymh.com>
2018-04-03 19:36:42 +1000

    Rm react-router-bootstrap

--- packages/fl-admin/src/components/Sidebar.js
+++ packages/fl-admin/src/components/Sidebar.js
@@ -1,15 +1,15 @@
 import _ from 'lodash' // eslint-disable-line
 import React from 'react'
 import {Link} from 'react-router'
-import {LinkContainer} from 'react-router-bootstrap'
-import {Nav, NavItem} from 'reactstrap'
+import {Nav, NavItem, NavLink} from 'reactstrap'
 import {modelAdmins} from '../index'
+
 
 // Links to all model index pages for the sidebar
 export default function Sidebar() {
 
   const links = _.map(modelAdmins, modelAdmin => (
-    <LinkContainer key={modelAdmin.path} to={modelAdmin.link()}><NavItem>{modelAdmin.plural}</NavItem></LinkContainer>
+    <NavItem key={modelAdmin.path}><NavLink tag={Link} to={modelAdmin.link()}>{modelAdmin.plural}</NavLink></NavItem>
   ))
 
   return (
