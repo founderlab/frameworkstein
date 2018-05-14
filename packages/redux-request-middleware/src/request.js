6a86cffba76ea6a156078f18b4ef1007c64d6b7d
gwilym <gwilym@gwilymh.com>
2018-05-14 14:54:30 +1000

    Update to react-router 4, add fetch-component-data, add redux-request-middleware

---
+++ packages/redux-request-middleware/src/request.js
@@ -0,0 +1,124 @@
+import _ from 'lodash'
+import retry from 'retry-unless'
+
+
+export function extractRequest(action) {
+  const {request, callback, parseResponse, ...rest} = action
+  return {request, callback, parseResponse, action: rest}
+}
+
+export function getEndFn(request) {
+  if (!request) return null
+
+  // Known api - superagent or orm
+  for (const methodName of ['toJSON', 'end']) {
+    const end = request[methodName]
+    if (_.isFunction(end)) return end.bind(request)
+  }
+
+  // Promise
+  if (_.isFunction(request.then)) return callback => request.then(res => callback(null, res)).catch(callback)
+
+  // Callback function
+  if (_.isFunction(request)) return request
+
+  return null
+}
+
+export function getError(err, res) {
+  if (err) {
+    if (err.text) {
+      let json
+      try {
+        json = JSON.parse(err.text)
+      }
+      catch (e) {
+        // noop
+      }
+      if (json && json.error) return json.error
+    }
+    return err
+  }
+  if (_.isUndefined(res)) return '[redux-request-middleware] No response received'
+  if (!res) return null
+  if (res.body && res.body.error) return res.body.error
+  if (res.ok === false) return res.body || res.status || '[redux-request-middleware] Unknown error: res.ok was false'
+  return null
+}
+
+const defaults = {
+  extractRequest,
+  getEndFn,
+  getError,
+  suffixes: {
+    START: '_START',
+    ERROR: '_ERROR',
+    SUCCESS: '_SUCCESS',
+  },
+  retry: {
+    times: 20,
+    interval: retryCount => Math.min(50 * (2**retryCount), 1000),
+  },
+  check: err => {
+    const status = err.status
+    if (status && status.toString()[0] === '4' || status === 500) return true
+    if (err.toString().match(/status (4|500)/)) return true
+    return false
+  },
+}
+
+// Wrap the end function to place the error status code on it if not present
+const wrapEnd = endFn => callback => {
+  endFn((err, res) => {
+    if (err && res && !err.status) {
+      err.status = res.status
+    }
+    callback(err, res)
+  })
+}
+
+export default function createRequestMiddleware(_options={}) {
+  const options = _.merge({}, defaults, _options)
+
+  return function requestMiddleware() {
+    return next => _action => {
+
+      const {request, callback, parseResponse, action} = options.extractRequest(_action)
+      const end = options.getEndFn(request)
+      if (!end) return next(action)
+
+      const {type, ...rest} = action
+      const START = type + options.suffixes.START
+      const ERROR = type + options.suffixes.ERROR
+      const SUCCESS = type + options.suffixes.SUCCESS
+
+      next({type: START, ...rest})
+
+      return new Promise((resolve, reject) => {
+        console.log('returnin promise', request)
+        const done = (err, res) => {
+          console.log('done', err, res)
+          const error = options.getError(err, res)
+          let finalAction = {}
+          if (error) {
+            finalAction = {res, error, type: ERROR, ...rest}
+          }
+          else {
+            finalAction = {res, type: SUCCESS, ...rest}
+            if (parseResponse) finalAction = parseResponse(finalAction)
+          }
+          next(finalAction)
+          if (callback) callback(error, finalAction)
+          if (error) reject(error)
+          else resolve(finalAction)
+        }
+
+        if (options.retry) {
+          return retry(options.retry, wrapEnd(end), options.check, done)
+        }
+        console.log('ending')
+        return end(done)
+      })
+    }
+  }
+}
---
+++ packages/redux-request-middleware/test/request.tests.js
@@ -0,0 +1,245 @@
+import assert from 'assert'
+import {spy} from 'sinon'
+import {createRequestMiddleware} from '../src'
+
+const suffixes = {
+  START: '_START',
+  ERROR: '_ERROR',
+  SUCCESS: '_SUCCESS',
+}
+const TYPE = 'ACTIONTYPE'
+
+function createSpy() {
+  return spy(action => {
+    assert.ok(action)
+    assert.ok(action.type === TYPE)
+  })
+}
+
+function createMiddlewareSpy() {
+  const nextFn = spy(action => {
+    assert.ok(action)
+
+    if (nextFn.calledOnce) {
+      assert.ok(action.type === TYPE + suffixes.START)
+    }
+    else if (nextFn.calledTwice) {
+      if (action.error || !action.res || action.res.ok === false) {
+        assert.ok(action.type === TYPE + suffixes.ERROR)
+      }
+      else {
+        assert.ok(action.res)
+        assert.ok(action.type === TYPE + suffixes.SUCCESS)
+      }
+    }
+  })
+  return nextFn
+}
+
+describe('requestMiddleware', () => {
+
+  it('Passes through an action without a request', () => {
+    const next = createSpy()
+    const action = {type: TYPE}
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+    assert.ok(next.calledOnce)
+  })
+
+  it('Passes through an action with a request field that isnt a function', () => {
+    const next = createSpy()
+    const action = {type: TYPE, request: 'lol'}
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+    assert.ok(next.calledOnce)
+  })
+
+  it('Passes through an action with a custom extractRequest method that isnt a function', () => {
+    const next = createSpy()
+    const action = {type: TYPE, req: 'lol', request: () => {}}
+    const middleware = createRequestMiddleware({
+      extractRequest: action => {
+        const {req, callback, ...rest} = action
+        return {request: req, callback, action: rest}
+      },
+      retry: false,
+    })
+    middleware()(next)(action)
+    assert.ok(next.calledOnce)
+  })
+
+  it('Calls a request', () => {
+    const req = {end: spy(callback => callback(null, {ok: true}))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Calls a request with retries', () => {
+    const times = 4
+    let called = 0
+    const req = {end: spy(callback => called++ < times-2 ? callback('Err') : callback(null, {ok: true}))}
+    const next = createMiddlewareSpy()
+
+    const action = {type: TYPE, request: req, callback: err => {
+      assert.ok(!err)
+      assert.ok(next.calledTwice)
+      assert.equal(req.end.callCount, times-2)
+    }}
+
+    const middleware = createRequestMiddleware({retry: {times}})
+    middleware()(next)(action)
+  })
+
+  it('Succeeds when res.ok isnt false', () => {
+    const req = {end: spy(callback => callback(null, [{json: 'yep'}]))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Calls a pure function request', () => {
+    const req = spy(callback => callback(null, {ok: true}))
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.calledOnce)
+  })
+
+  it('Custom parses a response', () => {
+    const req = {end: spy(callback => callback(null, {ok: true}))}
+    const next = createMiddlewareSpy()
+    const wrapper = action => {
+      if (action.type === TYPE + suffixes.SUCCESS) assert.equal(action.changed, 'yup')
+      next(action)
+    }
+    const action = {type: TYPE, request: req, parseResponse: action => ({changed: 'yup', ...action})}
+
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(wrapper)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Calls a request then calls a callback', () => {
+    const req = {end: spy(callback => callback(null, {ok: true}))}
+    const next = createMiddlewareSpy()
+    const callback = spy(err => {assert.ok(!err)})
+    const action = {callback, type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(callback.calledOnce)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Calls a request with config', () => {
+    const req = {next: spy(callback => callback(null, {ok: true}))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, aRequest: req}
+
+    const extractRequest = (action) => {
+      const {aRequest, ...rest} = action
+      return {request: aRequest, action: rest}
+    }
+    const getEndFn = request => request.next.bind(request)
+
+    const middleware = createRequestMiddleware({extractRequest, getEndFn, retry: false})
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.next.calledOnce)
+  })
+
+  it('Errors from an error callback', () => {
+    const req = {end: spy(callback => callback(new Error('failed')))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware({retry: false})
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Errors from an error callback after retries', () => {
+    const times = 4
+    const req = {end: spy(callback => callback(new Error('failed')))}
+    const next = createMiddlewareSpy()
+
+    const action = {type: TYPE, request: req, callback: err => {
+      assert.ok(err)
+      assert.ok(next.calledTwice)
+      assert.equal(req.end.callCount, times-1)
+    }}
+
+    const middleware = createRequestMiddleware({retry: {times, interval: 1}})
+    middleware()(next)(action)
+  })
+
+  it('Errors from an error body property', () => {
+    const req = {end: spy(callback => callback(null, {body: {error: 'failed'}}))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware()
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Errors from a bad status', () => {
+    const req = {end: spy(callback => callback(null, {ok: false, body: 'some stack trace'}))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware()
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Errors from a bad status without a body', () => {
+    const req = {end: spy(callback => callback(null, {ok: false, body: null, status: 500}))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware()
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+
+  it('Errors from a bad status without a body or status', () => {
+    const req = {end: spy(callback => callback(null, {ok: false, body: null, status: null}))}
+    const next = createMiddlewareSpy()
+    const action = {type: TYPE, request: req}
+
+    const middleware = createRequestMiddleware()
+    middleware()(next)(action)
+
+    assert.ok(next.calledTwice)
+    assert.ok(req.end.calledOnce)
+  })
+})
