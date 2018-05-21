b19ba61156424cc1d669aafc456b3e8b0801d4fb
gwilym <gwilym@gwilymh.com>
2018-05-21 12:35:19 +1000

    Link orms, fix exports

--- packages/fl-orm/src/index.js
+++ packages/fl-orm/src/index.js
@@ -1,9 +1,11 @@
 import createModel from './createModel'
+import Cursor from './Cursor'
 import Model from './Model'
 import Schema from './Schema'
 
 export {
   createModel,
+  Cursor,
   Model,
   Schema,
 }
