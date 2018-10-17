2032e8c46406ffdba90d6f3a4785e7d3ad27c27d
gwilym <gwilym@gwilymh.com>
2018-10-17 14:45:33 +1100

    Add canAccessAsync option to authorisation

--- packages/fl-auth-server/src/middleware/authorised.js
+++ packages/fl-auth-server/src/middleware/authorised.js
@@ -6,13 +6,27 @@ const defaults = {
 
 export default function createAuthMiddleware(options) {
   _.merge(options, defaults)
-  if (!options.canAccess) throw new Error(`[fl-auth] createAuthMiddleware: Missing options.canAccess. Got options: ${JSON.stringify(options)}`)
+  if (!options.canAccess && !options.canAccessAsync) throw new Error(`[fl-auth] createAuthMiddleware: Missing options.canAccess. Got options: ${JSON.stringify(options)}`)
 
-  return function authorised(req, res, next) {
-    options.canAccess({user: req.user, req, res}, (err, authorised, message) => {
-      if (err) return res.status(500).send({error: err})
-      if (!authorised) return res.status(401).send({error: message || options.unauthorisedMessage})
-      next()
-    })
+  return async function authorised(req, res, next) {
+    try {
+      const done = (err, authorised, message) => {
+        if (err) return res.status(500).send({error: err})
+        if (!authorised) return res.status(401).send({error: message || options.unauthorisedMessage})
+        next()
+      }
+
+      if (options.canAccess) return options.canAccess({user: req.user, req, res}, done)
+
+      // Result may be a boolean indicating success or an object containing err, authorised, message
+      const result = await options.canAccessAsync({user: req.user, req, res})
+
+      if (!result || _.isBoolean(result)) return done(null, !!result)
+      const {err, authorised, message} = result
+      done(err, authorised, message)
+    }
+    catch (err) {
+      res.status(500).send({error: err})
+    }
   }
 }
