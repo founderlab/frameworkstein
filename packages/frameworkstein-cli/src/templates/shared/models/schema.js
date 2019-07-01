1ae504e493a3da193dacb653e6aec0d71a818b44
gwilym <gwilym@gwilymh.com>
2019-07-01 11:48:05 +1000

    Formatting

--- packages/frameworkstein-cli/src/templates/shared/models/schema.js
+++ packages/frameworkstein-cli/src/templates/shared/models/schema.js
@@ -1,6 +1,5 @@
 export default (model) =>
-`export default {
-${model.fields.map(field => `  ${field.name}: '${field.type}',`).join('\n')}
+`export default {${model.fields.map(field => field.type ? `\n  ${field.name}: '${field.type}',` : '').join('')}
   updatedDate: 'DateTime',
   createdDate: 'DateTime',
 }
