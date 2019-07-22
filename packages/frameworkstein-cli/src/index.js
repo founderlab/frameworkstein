b7d958bef8abcfcd11469f75ad4cd911a115e02a
gwilym <gwilym@gwilymh.com>
2019-07-22 14:08:41 +1000

    Better fix

--- packages/frameworkstein-cli/src/index.js
+++ packages/frameworkstein-cli/src/index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+/* eslint-disable import/first */
+require('babel-core/register')
+require('babel-polyfill')
+
 import program from 'commander'
 import chalk from 'chalk'
 import fs from 'fs'
