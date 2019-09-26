f32cc9cddc61d08f72cbe7d72e2f32231e536d29
gwilym <gwilym@gwilymh.com>
2019-09-26 14:28:23 +1000

    Update redux-form

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,13 @@ module.exports = function(api) {
   api.cache(true)
 
   return {
-    "presets": ["@babel/preset-env", "@babel/preset-react"],
-    "plugins": [
-      ["@babel/plugin-proposal-decorators", {"legacy": true}],
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-transform-runtime",
-      "@babel/plugin-proposal-object-rest-spread",
-      "add-module-exports"
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', {legacy: true}],
+      ['@babel/plugin-proposal-class-properties', {loose : true}],
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-proposal-object-rest-spread',
+      'add-module-exports',
     ]
   }
 }
