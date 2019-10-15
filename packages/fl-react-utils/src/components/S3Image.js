f21bd422db27848b65f1263f25153ea17b100d13
gwilym <gwilym@gwilymh.com>
2019-10-15 13:04:27 +1100

    S3Image uses prop for s3 url

--- packages/fl-react-utils/src/components/S3Image.js
+++ packages/fl-react-utils/src/components/S3Image.js
@@ -7,16 +7,13 @@ import PropTypes from 'prop-types'
 export default class S3Image extends React.Component {
 
   static propTypes = {
+    s3Url: PropTypes.string.isRequired,
     filename: PropTypes.string.isRequired,
   }
 
-  static contextTypes = {
-    s3Url: PropTypes.string.isRequired,
-  }
-
   render() {
-    const { filename, ...rest } = this.props
-    const url = `${this.context.s3Url}/${filename}`
+    const { filename, s3Url, ...rest } = this.props
+    const url = `${s3Url}/${filename}`
 
     return (
       <img src={url} {...rest} />
