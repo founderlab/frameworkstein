62ace864550072c02c4d0c74c65580740d4d9306
gwilym <gwilym@gwilymh.com>
2020-01-16 15:59:17 +1100

    Accept a cacheKey option in addition to cacheKeyFromProps

--- packages/fl-redux-utils/src/hasCachedResults.js
+++ packages/fl-redux-utils/src/hasCachedResults.js
@@ -1,6 +1,6 @@
 import _ from 'lodash' // eslint-disable-line
 
 
-export default function hasCachedResults(state, cacheKey) {
-  return state.get('pagination') && state.get('pagination').get('cache') && state.get('pagination').get('cache').get(cacheKey)
+export default function hasCachedResults(state, cacheKey, paginationName='pagination') {
+  return state.get(paginationName) && state.get(paginationName).get('cache') && state.get(paginationName).get('cache').get(cacheKey)
 }
