93ac80b9af66e113b026d953ebdcd7fc9bf1e42a
gwilym <gwilym@gwilymh.com>
2023-03-28 10:42:33 +1100

    remove old style of pagination selector to prevent unnecessary rerendering

--- packages/fl-redux-utils/src/createPaginationReducer.js
+++ packages/fl-redux-utils/src/createPaginationReducer.js
@@ -1,82 +1,73 @@
-import _ from 'lodash' // eslint-disable-line
-import { fromJS } from 'immutable'
-import actionTypes from './actionTypes'
+import _ from 'lodash'
+import { createSelector } from 'reselect'
 
 
-export default function createPaginationReducer(_actionTypes, options={}) {
-  const { loadActions, deleteActions, saveActions, countActions, clearActions, cacheRestoreActions, cacheClearActions, setPageActions } = actionTypes(_actionTypes)
+function paginationState(state, paginateOn) {
+  if (_.isFunction(paginateOn)) return paginateOn(state)
+  return state[paginateOn]
+}
 
-  const defaultState = fromJS({
-    total: 0,
-    currentPage: 0,
-    ids: [],
-    pages: {},
-    cache: {},
+const defaultSelect = () => {}
+const defaults = {
+  modelsName: 'models',
+  paginationName: 'pagination',
+}
+
+export default function createPaginationSelector(paginateOn, selectState, _options) {
+  if (_.isPlainObject(selectState) && !_options) {
+    _options = selectState
+    selectState = defaultSelect
+  }
+  if (!selectState) selectState = defaultSelect
+  const options = _.defaults(_options || {}, defaults)
+
+  const selector = createSelector([
+    state => {
+      const pState = paginationState(state, paginateOn)
+      return pState && pState.get(options.modelsName)
+    },
+    state => {
+      const pState = paginationState(state, paginateOn)
+      if (!pState) return null
+      if (options.getPagination) return options.getPagination(pState)
+      return pState.get(options.paginationName)
+    },
+    (state, props) => options.cacheKey || (options.cacheKeyFromProps && options.cacheKeyFromProps(props)),
+    (_, props) => props && (props.itemsPerPage || props.itemsPerChunk),
+  ],
+  (models, _pagination, cacheKey, _itemsPerPage) => {
+    const cachedPagination = _pagination && _pagination.get('cache') && _pagination.get('cache').get(cacheKey)
+
+    const results = {
+      cached: !!cachedPagination,
+      visibleItems: [],
+      visibleIds: [],
+      totalItems: 0,
+      currentPage: 1,
+    }
+    const pagination = cachedPagination || _pagination
+    if (!pagination) return results
+
+    results.totalItems = +pagination.get('total')
+    results.currentPage = +pagination.get('currentPage')
+    const itemsPerPage = _itemsPerPage || options.perPage
+
+    if (pagination.get('append')) {
+      results.visibleIds = pagination.get('ids').toJSON()
+      if (itemsPerPage) results.visibleIds = results.visibleIds.slice(0, itemsPerPage * results.currentPage)
+    }
+    else {
+      const pageIdsIm = pagination.get('pages').get(pagination.get('currentPage').toString())
+      if (pageIdsIm) results.visibleIds = pagination.get('pages').get(pagination.get('currentPage').toString()).toJSON()
+    }
+
+    _.forEach(results.visibleIds, id => {
+      const m = models.get(id)
+      m && results.visibleItems.push(m.toJSON())
+    })
+
+    return results
   })
 
-  return function pagination(_state=defaultState, action={}) {
-    let state = _state
-
-    // Store the append option so selectors can see it
-    if (options.append && !state.get('append')) {
-      state = state.set('append', true)
-    }
-
-    if (_.includes(countActions, action.type)) {
-      state = state.merge({total: +action.res})
-    }
-    else if (_.includes(loadActions, action.type) && !_.isNil(action.page)) {
-      state = state.setIn(['pages', action.page.toString()], fromJS(action.ids))
-      state = state.set('ids', fromJS(_.uniq([...state.get('ids'), ...action.ids])))
-      state = state.set('currentPage', action.page)
-    }
-
-    // Remove deleted items without clearing everything
-    else if (_.includes(deleteActions, action.type)) {
-      const pages = state.get('pages').toJS()
-      const ids = state.get('ids').toJS()
-      const deletedId = action.deletedId || (action.model && action.model.id) || (action.deletedModel && action.deletedModel.id)
-      _.forEach(pages, (pageIds, page) => {
-        pages[page] = _.without(pageIds, deletedId)
-      })
-      state = state.merge({
-        pages,
-        ids: _.without(ids, deletedId),
-        cache: {},
-      })
-    }
-
-    // Clear all pagination and bail
-    else if (_.includes([...saveActions, ...clearActions], action.type)) {
-      return state.merge(defaultState.toJS())
-    }
-
-    // Restore state from cache key and bail
-    else if (action.cacheKey && _.includes(cacheRestoreActions, action.type)) {
-      const cachedState = state.get('cache').get(action.cacheKey) && state.get('cache').get(action.cacheKey).toJS()
-      // Return here so we don't cached the cached result again
-      return state.merge(cachedState)
-    }
-
-    // Clear cache and bail
-    else if (_.includes(cacheClearActions, action.type)) {
-      return state.merge({cache: {}})
-    }
-
-    // Clear cache and bail
-    else if (_.includes(setPageActions, action.type)) {
-      return state.set('currentPage', action.page)
-    }
-
-    if (action.cacheKey) {
-      const cachedState = {
-        pages: state.get('pages').toJS(),
-        total: state.get('total'),
-        currentPage: state.get('currentPage'),
-      }
-      state = state.setIn(['cache', action.cacheKey], fromJS(cachedState))
-    }
-
-    return state
-  }
+  return selector
 }
