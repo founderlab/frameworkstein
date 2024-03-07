15a70ee9864e37395d2e91bb329e9ae660c6c440
gwilym <gwilym@gwilymh.com>
2024-03-07 14:39:58 +1100

    allow onLogin to fail with a reason to be passed to the client

--- packages/fl-auth-server/src/strategies/Password.js
+++ packages/fl-auth-server/src/strategies/Password.js
@@ -1,29 +1,37 @@
 import LocalStrategy from './Local'
 
+
 // Strategy to log a user in using their username/password
 export default class PasswordStrategy extends LocalStrategy {
 
-  verify = async (req, email, password, callback) => {
+  async verify(req, email, password, callback) {
     try {
       const User = this.User
       const user = await User.findOne(this.userQuery(email))
+
       if (!user) {
         console.log('[fl-auth] email error: user not found', email)
         return callback(null, false, 'User not found')
       }
+
       if ((user.get('linkedInId') || user.get('linkedinId')) && !user.get('password')) {
         return callback(null, false, 'Registered by LinkedIn')
       }
+
       if (!user.passwordIsValid(password)) {
         return callback(null, false, 'Incorrect password')
       }
+
       if (User.onLogin) {
         try {
-          await User.onLogin({req, user})
+          const onLoginResult = await User.onLogin({ req, user })
+          if (onLoginResult && onLoginResult.invalid) {
+            return callback(null, false, onLoginResult.message)
+          }
         }
         catch (err) {
           console.log('[fl-auth] User.onLogin error', err, user)
-          callback(err, user)
+          return callback(err, user)
         }
       }
       callback(null, user)
